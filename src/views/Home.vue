19867e93e1d7ccaca24528a1435756351378a627
rajihawa <46829217+rajihawa@users.noreply.github.com>
2021-04-21 15:28:37 +0300

    added tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="bg-green-400">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
